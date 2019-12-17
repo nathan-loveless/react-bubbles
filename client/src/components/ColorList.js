7572e172ae28ea773fa45bc3b63ae5df8fcec909
Nathan Loveless <nathansl2003@yahoo.com>
2019-12-16 22:32:25 -0500

    Committing changes

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "./axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +22,23 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+      })
+      .catch(err => {
+        console.error(err);
+      });
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`colors/${color.id}`)
+    .then(res => {
+    })
+    .catch(err => console.error(err));
+
   };
 
   return (
